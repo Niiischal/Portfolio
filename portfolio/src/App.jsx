eb003cdbb26c9d7596d699367ec0325cf6d11d66
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-06 13:30:25 +0545

    adding floating navbar icons

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/header/Header";
+import NavBar from "./components/nav/NavBar";
 
 const App = () => {
   return (
     <>
       <Header />
+      <NavBar/>
     </>
   );
 };
--- portfolio/src/components/nav/NavBar.jsx
+++ portfolio/src/components/nav/NavBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { AiOutlineContacts, AiOutlineHome, AiOutlineProject, AiOutlineUser } from 'react-icons/ai'
 import './navbar.css'
 
 const NavBar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+        <a href="/"><AiOutlineHome/></a>
+        <a href="/about"><AiOutlineUser/></a>
+        <a href="/projects"><AiOutlineProject/></a>
+        <a href="/contact"><AiOutlineContacts/></a>
+    </nav>
   )
 }
 
