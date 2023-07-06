02842350c2b4eeecb9bba5bd1829ea6dfede9f0c
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-06 14:04:20 +0545

    adding active style to NavLink

--- portfolio/src/components/nav/NavBar.jsx
+++ portfolio/src/components/nav/NavBar.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
-import { AiOutlineContacts, AiOutlineHome, AiOutlineProject, AiOutlineUser } from 'react-icons/ai'
-import { NavLink } from 'react-router-dom'
-import './navbar.css'
-
+import React from "react";
+import {
+    AiOutlineContacts,
+    AiOutlineHome,
+    AiOutlineProject,
+    AiOutlineUser,
+} from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
 
 const NavBar = () => {
   return (
     <nav>
-        <NavLink to="/"><AiOutlineHome/></NavLink>
-        <NavLink to="/"><AiOutlineUser/></NavLink>
-        <NavLink to="/"><AiOutlineProject/></NavLink>
-        <NavLink to="/"><AiOutlineContacts/></NavLink>
+      <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          color: isActive ? "var(--color-light)" : "",
+          background: isActive ? "var(--color-bg)" : "",
+        })}
+      >
+        <AiOutlineHome />
+      </NavLink>
+      <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          color: isActive ? "var(--color-light)" : "",
+          background: isActive ? "var(--color-bg)" : "",
+        })}
+      >
+        <AiOutlineUser />
+      </NavLink>
+      <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          color: isActive ? "var(--color-light)" : "",
+          background: isActive ? "var(--color-bg)" : "",
+        })}
+      >
+        <AiOutlineProject />
+      </NavLink>
+      <NavLink
+        to="/"
+        style={({ isActive }) => ({
+          color: isActive ? "var(--color-light)" : "",
+          background: isActive ? "var(--color-bg)" : "",
+        })}
+      >
+        <AiOutlineContacts />
+      </NavLink>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
