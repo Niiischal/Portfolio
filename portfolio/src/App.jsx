e1b9f4513a1cb0039f7791e8285b7dddd641ce26
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-06 13:57:39 +0545

    adding css to the floating navbar and also installing react router dom

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/header/Header";
 import NavBar from "./components/nav/NavBar";
 
 const App = () => {
   return (
     <>
+    <Router>
       <Header />
       <NavBar/>
+    </Router>
     </>
   );
 };
--- portfolio/src/components/nav/NavBar.jsx
+++ portfolio/src/components/nav/NavBar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { AiOutlineContacts, AiOutlineHome, AiOutlineProject, AiOutlineUser } from 'react-icons/ai'
+import { NavLink } from 'react-router-dom'
 import './navbar.css'
+
 
 const NavBar = () => {
   return (
     <nav>
-        <a href="/"><AiOutlineHome/></a>
-        <a href="/about"><AiOutlineUser/></a>
-        <a href="/projects"><AiOutlineProject/></a>
-        <a href="/contact"><AiOutlineContacts/></a>
+        <NavLink to="/"><AiOutlineHome/></NavLink>
+        <NavLink to="/"><AiOutlineUser/></NavLink>
+        <NavLink to="/"><AiOutlineProject/></NavLink>
+        <NavLink to="/"><AiOutlineContacts/></NavLink>
     </nav>
   )
 }
